155ae875a13a5e1187f94780c326e1097ce3d621
otdoges <otdoges@proton.me>
2025-05-31 12:36:58 -0500

    forcing million to stop

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [MillionLint.vite({
-    enabled: true
+    enabled: false
   }), react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
